0647f0491596cead1aa544ccdd95b50bba79ff71
Nicholas Wade <beaker.wade@gmail.com>
2022-08-28 14:49:28 -0400

    Time: 1824 ms (5.00%), Space: 14.7 MB (25.33%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,5 +1,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
         t = list(t)
         for c in s:
             if c not in t:
