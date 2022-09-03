1d8989f4b370dfb2bdf3517b75f1905862c0c5c7
Nicholas Wade <beaker.wade@gmail.com>
2022-09-03 11:48:01 -0400

    Time: 52 ms (51.43%), Space: 13.9 MB (70.30%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -9,12 +9,13 @@ class Solution:
         left, right = dummy, head
         
         while n > 0:
-            right = right.next
+            right = right.next #this will put the right pointer n-1 positions in front of left pointer
             n -= 1
         
-        while right:
+        while right: #once right is null we know left is the n-2 node from the end of the list because left was already 1 behind right and 
+            #right is now in the n-1 position so left is n-2
             right = right.next
             left = left.next
         
-        left.next = left.next.next
-        return dummy.next
+        left.next = left.next.next #cut out left.next which is the n-1th node from the end of the list, this is because n is 1-indexed by the list is 0-indexed
+        return dummy.next #dummy isnt a node from the original list, dummy.next is the head node
