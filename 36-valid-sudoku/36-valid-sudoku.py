3900b259efb005cf57323fa66e8a21f97381bbe5
Nicholas Wade <beaker.wade@gmail.com>
2022-09-06 15:06:19 -0400

    Time: 173 ms (34.14%), Space: 14 MB (35.16%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,23 +1,10 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         
-        def init_mapper():
-            return {i: {} for i in range(9)}
-        
-        mapper_row = init_mapper()
-        mapper_col = init_mapper()
-        mapper_box = init_mapper()
-        pos_box = {
-            (0,0): 0,
-            (0,1): 1,
-            (0,2): 2,
-            (1,0): 3,
-            (1,1): 4,
-            (1,2): 5,
-            (2,0): 6,
-            (2,1): 7,
-            (2,2): 8,
-        }
+        mapper_row = {0: {}, 1: {}, 2: {}, 3: {}, 4: {}, 5: {}, 6: {}, 7: {}, 8: {}}
+        mapper_col = {0: {}, 1: {}, 2: {}, 3: {}, 4: {}, 5: {}, 6: {}, 7: {}, 8: {}}
+        mapper_box = {0: {}, 1: {}, 2: {}, 3: {}, 4: {}, 5: {}, 6: {}, 7: {}, 8: {}}
+        pos_box = {(0,0): 0, (0,1): 1, (0,2): 2, (1,0): 3, (1,1): 4, (1,2): 5, (2,0): 6, (2,1): 7, (2,2): 8}
         
         for r, row in enumerate(board):
             for c, item in enumerate(row):
