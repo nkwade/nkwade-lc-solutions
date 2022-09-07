ec5dc51fe62aab32e442f496e49314f56d3d3241
Nicholas Wade <beaker.wade@gmail.com>
2022-09-07 15:57:11 -0400

    Time: 105 ms (21.89%), Space: 18.7 MB (61.31%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.py
+++ 110-balanced-binary-tree/110-balanced-binary-tree.py
@@ -6,17 +6,17 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        self.res = True
+        self.res = True #assume the tree is balanced
         
         def depth(node):
-            if not node:
+            if not node:#if the node is null then do not add it to the depth
                 return 0
-            l_depth = depth(node.left)
-            r_depth = depth(node.right)
+            l_depth = depth(node.left) #find the left depth
+            r_depth = depth(node.right)#right the right depth
             
-            if abs(l_depth - r_depth) > 1:
-                self.res = False
+            if abs(l_depth - r_depth) > 1:#if the difference in depths is greater than 1 then it is not balanced
+                self.res = False #tree is not balanced
             
-            return 1 + max(l_depth, r_depth)
-        depth(root)
-        return self.res
+            return 1 + max(l_depth, r_depth) #add the current node plus the max of the left and right depths
+        depth(root)#call function
+        return self.res#return if the tree is balanced
