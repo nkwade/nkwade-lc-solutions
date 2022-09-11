915fcadd942873443a05b62c47a0c4a6bc379f8a
Nicholas Wade <beaker.wade@gmail.com>
2022-09-11 14:05:00 -0400

    Time: 9618 ms (7.63%), Space: 14 MB (12.37%) - LeetHub

---
+++ 79-word-search/79-word-search.py
@@ -0,0 +1,28 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        def search(k, i, j):
+            if k == len(word):
+                return True
+            if(i<0 or j<0 or i==len(board) or j==len(board[0]) or board[i][j]!=word[k]):
+                return False
+            ch=board[i][j]
+            board[i][j]="#"
+            opt1=search(k+1, i+1,j)
+            opt2=search(k+1, i-1,j)
+            opt3=search(k+1, i,j+1)
+            opt4=search(k+1, i,j-1)
+            board[i][j]=ch
+            return(opt1 or opt2 or opt3 or opt4)
+        
+        def f(board,word,n,m):
+            for i in range(0,n):
+                for j in range(0,m):
+                    if(board[i][j]==word[0]):
+                        if(search(0,i,j)):
+                            return True
+            return False
+        n=len(board)
+        m=len(board[0])
+        return(f(board,word,n,m))
+        return search(0, 0, 0)
